1c05265bacc5852ed2bc69393be8a5709300e50b
Daffa2101 <daffaakmal2101@gmail.com>
2025-05-26 21:59:11 +0700

    feat: implemented directory and directory detail page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import ArticlePage from '../pages/ArticlePage';
+import DirectoryPage from '../pages/DirectoryPage';
+import DirectoryDetailPage from '../pages/DirectoryDetailPage';
+import '../styles/DirectoryPage.css';
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/article/:id" element={<ArticlePage />} />
+    <Route path="/directory" element={<DirectoryPage />} />
+    <Route path="/directory/:id" element={<DirectoryDetailPage />} />
     {/* Add more routes as needed */}
   </Routes>
 );
