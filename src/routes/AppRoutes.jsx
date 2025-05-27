d39cd87a5263491053bdb03a51eb6a3593159c3b
NIZ29-PC\Nisrina <nisrinaninis03@gmail.com>
2025-05-28 02:00:34 +0700

    feat(routes): add new routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,16 +6,23 @@ import CommunityPage from '../pages/ComunityPage';
 import RegisterPage from '../pages/RegisterPage';
 import JoinGroupPage from '../pages/JoinGroupPage';
 import CommunityNamePage from '../pages/CommunityNamePage';
+import SignInPage from '../pages/SignInPage';
+import SignUpPage from '../pages/SignUpPage';
+import PasswordManager from '../pages/PasswordManager';
+PasswordManager
 
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/article/:id" element={<ArticlePage />} />
-    <Route path="/comunity" element={<CommunityPage />} />
+    <Route path="/community" element={<CommunityPage />} />
     <Route path="/register" element={<RegisterPage />} />
     <Route path="/joingroup/:id" element={<JoinGroupPage />} />
     <Route path="/communityname/:id" element={<CommunityNamePage />} />
+    <Route path="/signin" element={<SignInPage />} />
+    <Route path="/signup" element={<SignUpPage />} />
+    <Route path="/passwordmanager" element={<PasswordManager />} />
     {/* Add more routes as needed */}
   </Routes>
 );
